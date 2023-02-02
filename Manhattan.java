7b5cabfb936c3610ecd741105fb4c71868acc2a4
Dmitri Stanchevici <dmitristanchevici@Dmitris-iMac.local>
2023-02-02 09:47:59 -0500

    Manhattan for 4.29

---
+++ Manhattan.java
@@ -0,0 +1,37 @@
+// Unit 3, Module 4
+
+public class Manhattan {
+
+    public static void main (String[] arv)
+    {
+
+	int r = 1, c = 1;
+	int n = countPaths (r, c);
+	System.out.println ("r=" + r + " c=" + c + " -> n=" + n);
+
+	r = 2;
+	c = 2;
+	n = countPaths (r, c);
+	System.out.println ("r=" + r + " c=" + c + " -> n=" + n);
+
+	r = 5;
+	c = 7;
+	n = countPaths (r, c);
+	System.out.println ("r=" + r + " c=" + c + " -> n=" + n);
+
+    }
+
+    static int countPaths (int numRows, int numCols)
+    {
+	if ( (numRows == 0) || (numCols == 0) ) {
+	    return 1;
+	}
+
+	int downCount = countPaths (numRows-1, numCols);
+//	System.out.println ("downCount: downCount=" + downCount);
+	int rightCount = countPaths (numRows, numCols-1);
+//	System.out.println ("rightCount: downCount=" + downCount + " rightCount=" + rightCount );
+	return (downCount + rightCount);
+    }
+
+}
